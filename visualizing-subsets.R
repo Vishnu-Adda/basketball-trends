d9f56816c1a18d1febbf03d9278be98fbfa19396
Vishnu Adda <addavishnu@gmail.com>
2018-06-24 22:18:41 -0700

    Add some various visualizations.

--- visualizing-subsets.R
+++ visualizing-subsets.R
@@ -10,4 +10,32 @@ myplot <- function(data, rows=1:10) {
 }
 
 myplot(MinutesPlayed/Games, 3)
+myplot(Games)
+
+#Salary ( Shouldn't relate Salary w/ in-game stats)
+myplot(Salary)
+myplot(Salary / Games)
+myplot(Salary / FieldGoals)
+
+#In-Game Metrics
+myplot(MinutesPlayed)
+myplot(Points)
+
+#In-Game Metrics Normalized
+myplot(FieldGoals/Games)
+myplot(FieldGoals/FieldGoalAttempts)
+myplot(FieldGoalAttempts/Games)
+myplot(Points/Games)
+
+#Interesting Observation
+myplot(MinutesPlayed/Games)
+myplot(Games)
+
+#Time is valuable
+myplot(FieldGoals/MinutesPlayed)
+
+#Player Style
+myplot(Points/FieldGoals)
+
+
 
